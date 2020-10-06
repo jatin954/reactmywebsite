f749e733474b6cdb6dfc0b4ad0a27979fbd7cfbc
jatin954 <thakurjatin891@gmail.com>
2020-10-06 16:36:12 +0530

    my first commit reactwebsite

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+  <>
+   <div className='header'> 
+   <h1> Thakur Keep </h1>
+   </div>
+
+  </>
+  );
+};
+
+export default Header;
